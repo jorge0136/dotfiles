ff95f4c06660b3e23206d2542569cf2055a36984
Thomas George <jorge0136@gmail.com>
2021-01-25 21:31:01 -0800

    Perfects syntax of .railsrc

--- rails_template.rb
+++ rails_template.rb
@@ -1,15 +1,17 @@
-group :development, :test do
+gem_group :development, :test do
   gem "fabrication"
   gem "guard-rspec"
   gem "pry-byebug"
   gem "rails-controller-testing", "~> 1.0"
   gem "rspec-rails"
   gem "rubocop-rspec", require: false
+end
 
-group :development do
+gem_group :development do
   gem "optimum-energy-rubocop"
   gem "rubocop-rspec", require: false
   gem "simplecov"
   gem "simplecov-console"
+end
 
 run "bundle install"
